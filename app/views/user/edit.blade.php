f7fdab4f25b192caa51b17750091e1df589cd3d8
Nick Kotenberg <happilymarrieddad@yahoo.com>
2014-09-26 13:29:07 -0600

    finished user edit page

--- app/views/user/edit.blade.php
+++ app/views/user/edit.blade.php
@@ -21,7 +21,7 @@
             <input type="text" id="first_name" name="first_name" class="form-control" value="{{Auth::user()->first_name}}" />
         </div>
         <div class="form-group">
-            <label for="last_name">First Name</label>
+            <label for="last_name">Last Name</label>
             <input type="text" id="last_name" name="last_name" class="form-control" value="{{Auth::user()->last_name}}" />
         </div>
         <div class="form-group">
